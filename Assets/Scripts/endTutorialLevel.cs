a20b07e35399ec3bab4056390e98c767dbaee44f
modijani <mohammadkhan.mail@gmail.com>
2023-02-07 10:51:15 +0100

    Added abstract script for endLevel

---
+++ Assets/Scripts/endTutorialLevel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class endTutorialLevel : abstractEndLevel
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        sceneName = "TutorialScene";
+    }
+
+}
