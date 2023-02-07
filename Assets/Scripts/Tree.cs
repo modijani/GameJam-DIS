a5da72f1fe974e415364a78770bd2c833bd719b2
Kerry <kjn45@case.edu>
2023-02-07 06:40:10 -0500

    Scene3 Setup

---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : Obstacle
+{
+
+    protected override void setDamage()
+    {
+        damage = 10;
+    }
+}
