c69445f9ecfb1a8cea2ea246ccade8e5768ad902
annashansen <a.s.hansen@wustl.edu>
2023-02-07 14:25:49 +0100

    Level 4

---
+++ Assets/Scripts/endLevel4.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endLevel4 : abstractEndLevel
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        sceneNameToChangeInto = "TutorialScene";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
