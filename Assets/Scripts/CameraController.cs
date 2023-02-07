ee18f9c3c9a157e22fc79adf35a6c18c6a8283ce
modijani <mohammadkhan.mail@gmail.com>
2023-02-07 12:08:35 +0100

    Added a blur effect

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,20 +2,97 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+public class CameraController : MonoBehaviour
+{   
+  
+
+}*/
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
-    public Transform player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform playerFollow;
+    public GameObject player;
+
+    public static bool doBlur = false;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
-        pos.y = player.position.y;
+        pos.y = playerFollow.position.y;
         transform.position = pos;
+
+
+    }
+
+    [Tooltip("The Blur Shader attahed with this Asset.")]
+    public Shader blurShader;
+
+    [Tooltip("The Blur Material created using the Blur Script.")]
+    public Material blurMaterial;
+
+    [Tooltip("Strength of the Blur. Governs the Area of Pixels to Blend.")]
+    [Range(0f, 10f)]
+    public float radius = 3.0f;
+
+    [Tooltip("Iterations of Blur Calculation.")]
+    [Range(1, 6)]
+    public int qualityIterations = 2;
+
+    [Tooltip("Improves quality and reduces sharp edges (Downsamples the Image).")]
+    [Range(0, 3)]
+    public int filter = 1;
+
+    void OnRenderImage(RenderTexture sourcert, RenderTexture destinationrt)
+    {
+        
+        if (blurShader == null)
+        {
+            return;
+        }
+
+        if (!(player.GetComponent<Player>().drunkLevel > 0))
+        {
+            Graphics.Blit(sourcert, destinationrt);
+            return;
+        }
+
+        float widthModification = 1.0f / (1.0f * (1 << filter));
+
+        blurMaterial.SetVector("_Param", new Vector4(radius * widthModification, -radius * widthModification, 0f, 0f));
+        sourcert.filterMode = FilterMode.Bilinear;
+
+        int rendertextureWidth = sourcert.width >> filter;
+        int rendertextureHeight = sourcert.height >> filter;
+
+        RenderTexture rendertexture = RenderTexture.GetTemporary(rendertextureWidth, rendertextureHeight, 0, sourcert.format);
+
+        rendertexture.filterMode = FilterMode.Bilinear;
+        Graphics.Blit(sourcert, rendertexture, blurMaterial, 0);
+
+        for (int i = 0; i < qualityIterations; i++)
+        {
+            float iterationOffset = (i * 1.0f);
+            blurMaterial.SetVector("_Param", new Vector4(radius * widthModification + iterationOffset, -radius * widthModification - iterationOffset, 0.0f, 0.0f));
+            RenderTexture rendertexturetemp = RenderTexture.GetTemporary(rendertextureWidth, rendertextureHeight, 0, sourcert.format);
+            rendertexturetemp.filterMode = FilterMode.Bilinear;
+            Graphics.Blit(rendertexture, rendertexturetemp, blurMaterial, 1);
+            RenderTexture.ReleaseTemporary(rendertexture);
+            rendertexture = rendertexturetemp;
+            rendertexturetemp = RenderTexture.GetTemporary(rendertextureWidth, rendertextureHeight, 0, sourcert.format);
+            rendertexturetemp.filterMode = FilterMode.Bilinear;
+            Graphics.Blit(rendertexture, rendertexturetemp, blurMaterial, 2);
+            RenderTexture.ReleaseTemporary(rendertexture);
+            rendertexture = rendertexturetemp;
+        }
+        Graphics.Blit(rendertexture, destinationrt);
+        RenderTexture.ReleaseTemporary(rendertexture);
+        
+
     }
 }
+
+
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     public float verticalSpeed = 0.05f;
     public float horizontalSpeed = 0.5f;
     public float healthOfPlayer = 100;
+    public float drunkLevel = 0f;
 
     void Start()
     {
